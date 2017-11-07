c53b118aaebd3747e4e90ee52cc9effa9b502627
Phantom2017 <csharpsolutionsuk@gmail.com>
2017-11-07 04:36:22 +0000

    Extract Text and Uglify Plugins added

--- ASPNetCoreReact/ASPNetCoreReact/webpack.config.js
+++ ASPNetCoreReact/ASPNetCoreReact/webpack.config.js
@@ -1,5 +1,7 @@
 ﻿const path = require('path');
 const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const extractCSS = new ExtractTextPlugin('allstyles.css');
 
 module.exports = {
     entry: './wwwroot/source/app.js',
@@ -8,16 +10,18 @@ module.exports = {
         filename: 'bundle.js'
     },
     plugins: [
+        extractCSS,
         new webpack.ProvidePlugin({
             $: 'jquery',
             jQuery: 'jquery',
             'window.jQuery': 'jquery',
             Popper: ['popper.js', 'default']
-        })
+        }),
+        new webpack.optimize.UglifyJsPlugin()
     ],
     module: {
         rules: [
-            { test: /\.css$/, use: [{ loader: "style-loader" }, { loader: "css-loader" }]},
+            { test: /\.css$/, use: extractCSS.extract(['css-loader?minimize']) },
             { test: /\.js?$/, use: { loader: 'babel-loader', options: { presets: ['@babel/preset-env'] } } },
         ]
     }
